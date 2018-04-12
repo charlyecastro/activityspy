3050667aee811d48bb40014a0e9629a7f2f5f461
charlyecastro <castroc@uw.edu>
2018-04-11 19:12:51 -0700

    added testing logs

--- activityspy/app/src/main/java/edu/washington/castroc/activityspy/MainActivity.kt
+++ activityspy/app/src/main/java/edu/washington/castroc/activityspy/MainActivity.kt
@@ -2,11 +2,33 @@ package edu.washington.castroc.activityspy
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+   companion object {
+       private val TAG = "MainActivity"
+   }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i(TAG,"onCreate is bithy!!" )
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart is working!!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume is doing its job!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+            Log.e(TAG, "were going down caption")
     }
 }
